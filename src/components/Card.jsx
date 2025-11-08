d88a442004275bfb4123827d3e75498e2d1a2214
Niraj Verma <vermaniraj9607@gmail.com>
2025-11-08 23:31:50 +0530

    Version 2 Completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
-import React from "react";
 import { FaRegFileLines } from "react-icons/fa6";
-import { LuDownload } from "react-icons/lu";
 import { IoIosClose } from "react-icons/io";
 import { motion, scale } from "motion/react";
 
-const Card = ({ data, reference }) => {
+const deleteTask = (index) => {
+  const copyData = [...data];
+  copyData.splice(index, 1);
+  setData(copyData);
+};
+
+const Card = ({ data, reference, onDelete }) => {
   return (
     <motion.div
       drag
@@ -15,23 +19,30 @@ const Card = ({ data, reference }) => {
       className="relative w-60 h-72 rounded-[30px] bg-zinc-900 text-white py-8 px-6 overflow-hidden shrink-0"
     >
       <FaRegFileLines />
-      <p className="text-[16px] mt-5 leading-tight">{data.desc}</p>
+      <p className="text-[16px] mt-5 leading-tight">{data.taskName}</p>
       <div className="footer absolute bottom-0 left-0  w-full">
         <div className="flex items-center justify-between mb-3 py-3 px-8">
-          <h5>{data.filesize}</h5>
-          <span className="w-6 h-6 bg-sky-200 rounded-full flex items-center justify-center">
-            {data.close ? (
-              <IoIosClose size="1.5em" color="#000" />
-            ) : (
-              <LuDownload size="0.9em" color="#000" />
-            )}
+          <h5>Delete</h5>
+          <span
+            onClick={() => {
+              onDelete(data.index);
+            }}
+            className="w-6 h-6 bg-red-500 rounded-full flex items-center justify-center"
+          >
+            <IoIosClose size="1.5em" color="#000" />
           </span>
         </div>
-        {data.tag.isOpen && (
+        {data.priority && (
           <div
-            className={`tag w-full py-4 ${data.tag.tagColor} flex items-center justify-center`}
+            className={`tag w-full py-4 ${
+              data.priority === "High"
+                ? "bg-red-500"
+                : data.priority === "Medium"
+                ? "bg-yellow-500"
+                : "bg-green-500"
+            } flex items-center justify-center`}
           >
-            <h3 className="text-md">{data.tag.tagTitle}</h3>
+            <h3 className="text-md">{data.priority}</h3>
           </div>
         )}
       </div>
